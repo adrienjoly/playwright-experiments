ce9d5df408cc02eb8329bd160b69ef13aa4b39fd
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-09-13 10:31:03 +0200

    add sample tests

---
+++ tests/dummy-form/index.js
@@ -0,0 +1,21 @@
+"use strict";
+const TARGET = Math.round(100 * Math.random());
+document.getElementById("gameForm").addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const guess = document.getElementById("number").value; // TODO: case with parseInt
+  document.getElementById("number").value = "";
+  if (guess > TARGET) {
+    document.getElementById("feedback").textContent =
+      "Your guess is bigger than the target";
+  } else if (guess < TARGET) {
+    document.getElementById("feedback").textContent =
+      "Your guess is smaller than the target";
+  }
+  // eslint-disable-next-line eqeqeq
+  else if (guess == TARGET) {
+    document.getElementById("feedback").textContent =
+      "Congrats! The number was " + TARGET;
+  } else {
+    throw new Error(guess + "is not a number");
+  }
+});
