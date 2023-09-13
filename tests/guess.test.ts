7be7f06a6c3e7890c8334ca5b91d679cf2242cdb
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-09-13 14:05:36 +0200

    for each possible number, run one test + show corresponding coverage in stdout

--- tests/guess.test.ts
+++ tests/guess.test.ts
@@ -3,41 +3,58 @@ import type { Protocol } from "playwright-core/types/protocol.d.ts";
 
 type ScriptId = string;
 
-const sourceFiles = new Map<
-  ScriptId,
-  Protocol.Debugger.getScriptSourceReturnValue
->();
+type SourceCode = string;
 
-test("guess the number", async ({ page }) => {
-  // cf https://playwright.dev/docs/api/class-cdpsession
-  const client = await page.context().newCDPSession(page);
-  await client.send("Debugger.enable");
+const sourceFiles = new Map<ScriptId, SourceCode>();
 
-  // cf https://chromedevtools.github.io/devtools-protocol/
-  client.on("Debugger.scriptParsed", async (opts) => {
-    if (opts.scriptLanguage !== "JavaScript") return;
-    if (opts.url === "") return;
-    console.log(
-      `${opts.url} ${opts.startLine}:${opts.startColumn} -> ${opts.endLine}:${opts.endColumn}`
-    );
-    const source = await client.send("Debugger.getScriptSource", {
-      scriptId: opts.scriptId,
+for (let number = 0; number <= 10; ++number) {
+  test(`try number ${number}`, async ({ page }) => {
+    // cf https://playwright.dev/docs/api/class-cdpsession
+    const client = await page.context().newCDPSession(page);
+    await client.send("Debugger.enable");
+
+    // cf https://chromedevtools.github.io/devtools-protocol/
+    client.on("Debugger.scriptParsed", async (opts) => {
+      if (opts.scriptLanguage !== "JavaScript") return;
+      if (opts.url === "") return; // TODO: reconnaitre aussi les scripts inline au html
+
+      console.log(
+        `${opts.url} ${opts.startLine}:${opts.startColumn} -> ${opts.endLine}:${opts.endColumn}`
+      );
+      const source = await client.send("Debugger.getScriptSource", {
+        scriptId: opts.scriptId,
+      });
+      sourceFiles.set(opts.scriptId, source.scriptSource);
     });
-    sourceFiles.set(opts.scriptId, source);
+
+    await page.goto("http://localhost:8080/");
+
+    await client.send("Profiler.enable");
+    await client.send("Profiler.startPreciseCoverage", {
+      callCount: true,
+      detailed: true,
+    });
+
+    await page.getByRole("textbox").type(number.toString());
+    await page.getByRole("button").click();
+
+    const { result } = await client.send("Profiler.takePreciseCoverage");
+    result.forEach((scriptCoverage) => {
+      const code = sourceFiles.get(scriptCoverage.scriptId);
+      if (code) {
+        const ranges = scriptCoverage.functions[0].ranges.map(
+          (range) =>
+            `(${range.count}) ${getCode(code, range)
+              .replace(/\n/g, " ")
+              .substring(0, 80)}`
+        );
+
+        console.log({ number, ranges });
+      }
+    });
   });
 
-  await client.send("Profiler.enable");
-  await client.send("Profiler.startPreciseCoverage");
-  await page.goto("http://localhost:8080/");
-
-  await page.getByRole("textbox").type("50");
-
-  await page.getByRole("button").click();
-
-  const { result } = await client.send("Profiler.takePreciseCoverage");
-  result.forEach(
-    (c) =>
-      sourceFiles.has(c.scriptId) &&
-      console.log(c.functions.map((f) => f.ranges))
-  );
-});
+  function getCode(code: SourceCode, { startOffset, endOffset }) {
+    return code.substring(startOffset, endOffset);
+  }
+}
