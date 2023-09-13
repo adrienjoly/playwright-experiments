37971590f75dbdee49bb6f00cb711776f521b5b7
Adrien Joly <531781+adrienjoly@users.noreply.github.com>
2023-09-13 10:47:44 +0200

    track `Debugger.scriptParsed` events from CDP (chrome debug protocol) cf https://playwright.dev/docs/api/class-cdpsession and https://chromedevtools.github.io/devtools-protocol/

--- tests/guess.test.ts
+++ tests/guess.test.ts
@@ -1,7 +1,22 @@
 import { test, expect } from "@playwright/test";
 
 test("guess the number", async ({ page }) => {
+  // cf https://playwright.dev/docs/api/class-cdpsession
+  const client = await page.context().newCDPSession(page);
+  await client.send("Debugger.enable");
+
+  // cf https://chromedevtools.github.io/devtools-protocol/
+  client.on("Debugger.scriptParsed", (opts) =>
+    console.log("Debugger.scriptParsed", opts)
+  );
+
   await page.goto("http://localhost:8080/");
+
+  // const response = await client.send("Animation.getPlaybackRate");
+  // console.log("playback rate is " + response.playbackRate);
+  // await client.send("Animation.setPlaybackRate", {
+  //   playbackRate: response.playbackRate / 2,
+  // });
 
   await page.getByRole("textbox").type("50");
 
